6086891c18c6d41f791f03e0dc4a8f1f016c79a1
christocarr <christocarr@outlook.com>
2021-02-11 18:09:49 +0000

    Fix: firebase app duplicate error

--- firebase.js
+++ firebase.js
@@ -1,14 +1,19 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-const app = firebase.initializeApp({
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-});
+let app;
 
-export const auth = app.auth();
+//condition needed to check if a firebase app already exists
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp({
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+  });
+}
+
+export const appAuth = app.auth();
 export default app;
