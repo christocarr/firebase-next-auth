052ede4daac1a73145050a01d7995f7ffc4b460f
christocarr <christocarr@outlook.com>
2021-02-11 17:05:00 +0000

    Fix: enviroment variables

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    API_KEY: 'AIzaSyAPNRbS4za7P06zgDn0T4Rf5hwXdBLrLhM',
+    AUTH_DOMAIN: 'dev-auth-firebase-8285d.firebaseapp.com',
+    PROJECT_ID: 'dev-auth-firebase-8285d',
+    STORAGE_BUCKET: 'dev-auth-firebase-8285d.appspot.com',
+    MESSAGING_SENDER_ID: '290364892258',
+    APP_ID: '290364892258:web:46609e31ea23bf36b09d3b',
+  },
+};
