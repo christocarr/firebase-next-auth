26fc881548f890f4e989967794691719dcae9526
christocarr <christocarr@outlook.com>
2021-02-12 11:59:56 +0000

    fix: auth not defined error

--- firebase.js
+++ firebase.js
@@ -1,19 +1,19 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-let app;
+export const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
 
-//condition needed to check if a firebase app already exists
-if (firebase.apps.length === 0) {
-  app = firebase.initializeApp({
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-  });
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
 }
 
+const app = firebase.app();
+
 export const appAuth = app.auth();
-export default app;
