ec806eaf4bf1ee759d8de38e1c26552ec79ad6b8
christocarr <christocarr@outlook.com>
2021-02-13 11:02:33 +0000

    wip: pages for login and register

---
+++ pages/login.js
@@ -0,0 +1,11 @@
+import Login from '../components/Login';
+
+function LoginPage() {
+  return (
+    <div className="m-auto w-5/6 max-w-sm h-screen flex flex-col justify-center items-center">
+      <Login />
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ pages/register.js
@@ -0,0 +1,11 @@
+import Register from '../components/Register';
+
+function RegisterPage() {
+  return (
+    <div className="m-auto w-5/6 max-w-sm h-screen flex flex-col justify-center items-center">
+      <Register />
+    </div>
+  );
+}
+
+export default RegisterPage;
