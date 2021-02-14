393647461e0fc731307a05fad6b0f326e655925f
christocarr <christocarr@outlook.com>
2021-02-14 14:21:41 +0000

    wip: layout component for site

---
+++ components/Layout/Layout.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <title>Pet Snap</title>
+        <meta charSet="utf" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <main className="w-screen h-screen flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-center w-5/6 max-w-sm">
+          {children}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Layout;
