308db2c75eadb232f48972e8a5f415094debebdf
christocarr <christocarr@outlook.com>
2021-02-17 15:55:01 +0000

    wip: add header component

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Header from '../Layout/Header';
 
 function Layout({ children }) {
   return (
@@ -8,6 +9,7 @@ function Layout({ children }) {
         <meta charSet="utf" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+      <Header />
       <main className="w-screen h-screen flex flex-col justify-center items-center">
         <div className="flex flex-col justify-center items-center w-5/6 max-w-sm">
           {children}
