538788ff2eb86490de6a809cc0224109949f1eb3
christocarr <christocarr@outlook.com>
2021-02-17 16:06:54 +0000

    wip: only show navigation when user is signed in

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,17 +1,29 @@
 import Link from 'next/link';
+
+import { useAuth } from '../../context/authContext';
+
 function Header() {
+  const { currentUser } = useAuth();
   return (
     <header className="flex justify-between items-center h-20 p-5 border-b border-gray-200">
-      <div>
-        <Link href="/">LOGO</Link>
-      </div>
-      <nav className>
-        <ul>
-          <li>
-            <Link href="/profile">Profile</Link>
-          </li>
-        </ul>
-      </nav>
+      {/* only have clickabel logo when a user is signed in */}
+      {currentUser ? (
+        <div>
+          <Link href="/">LOGO</Link>
+        </div>
+      ) : (
+        <div>LOGO</div>
+      )}
+      {/* only show nav when a user is signed in */}
+      {currentUser && (
+        <nav>
+          <ul>
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 }
