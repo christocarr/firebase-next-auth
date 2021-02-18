4f7de17af7ff5861db68c9695c1e802dc322bbf6
christocarr <christocarr@outlook.com>
2021-02-18 16:50:06 +0000

    wip: image upload form

--- components/ImageUpload/ImageUpload.js
+++ components/ImageUpload/ImageUpload.js
@@ -1,1 +1,45 @@
+import { useState } from 'react';
+import { Form, Label, Input, FormButton } from '../Form/';
+import Progress from '../Progress';
 
+function ImageUpload() {
+  const [imageFile, setImageFile] = useState(null);
+  const [error, setError] = useState('');
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const types = ['image/png', 'image/jpeg', 'image/jpg'];
+
+  const handleChange = (ev) => {
+    setError('');
+    const file = ev.target.files[0];
+    if (file && types.includes(file.type)) {
+      setImageFile(file);
+    } else {
+      setImageFile(null);
+      setError('File type is invalid. Please choose a png or jpg file.');
+    }
+  };
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    setSelectedFile(imageFile);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      {selectedFile && (
+        <Progress file={selectedFile} setFile={setSelectedFile} />
+      )}
+      <Label>Image file</Label>
+      <Input type="file" onChange={handleChange} />
+      {error ? (
+        <div className="bg-red-200 p-1 mb-2">
+          <p className="text-center text-red-600 mb-1 font-semibold">{error}</p>
+        </div>
+      ) : null}
+      <FormButton type="submit">Upload</FormButton>
+    </Form>
+  );
+}
+
+export default ImageUpload;
