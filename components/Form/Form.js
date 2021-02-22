8dc9a305804f336efffa3659de9053849ac4a513
christocarr <christocarr@outlook.com>
2021-02-22 15:51:07 +0000

    wip: change size on larger screens

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -1,7 +1,7 @@
 function Form({ children, onSubmit }) {
   return (
     <form
-      className="flex flex-col rounded-md py-6 px-4 mb-3 w-full border-gray-100 border shadow-xl sm:w-2/3"
+      className="flex flex-col rounded-md py-6 px-4 mb-3 w-5/6 bg-white border-gray-100 border shadow-xl sm:max-w-sm"
       onSubmit={onSubmit}
     >
       {children}
