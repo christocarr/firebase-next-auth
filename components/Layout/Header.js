c735055650ea41be005570696bc9f0d0fbc07653
christocarr <christocarr@outlook.com>
2021-02-26 10:03:05 +0000

    wip: add dropdown for profile button

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import Link from 'next/link';
-
 import { Context } from '../../context/Context';
+import { SignOut } from '../Auth';
 
 function Header() {
+  const [openDropdown, setOpenDropdown] = useState(false);
   const { currentUser, toggleModal } = Context();
+
+  const handleOpenDropdown = () => {
+    setOpenDropdown((prevState) => !prevState);
+  };
+
   return (
     <header className="flex justify-between items-center h-20 p-5 border-b border-gray-200">
       {/* only have clickabel logo when a user is signed in */}
@@ -19,7 +26,18 @@ function Header() {
         <nav className="w-44">
           <ul className="flex flex-row-reverse justify-between">
             <li>
-              <Link href="/profile">Profile</Link>
+              {/* <Link href="/profile">Profile</Link> */}
+              <button onClick={handleOpenDropdown}>Profile</button>
+              {openDropdown && (
+                <ul className="flex flex-col absolute">
+                  <li>
+                    <Link href="/profile">Profile</Link>
+                  </li>
+                  <li>
+                    <SignOut />
+                  </li>
+                </ul>
+              )}
             </li>
             <li>
               {/* <Link href="/image-upload">Add image</Link> */}
