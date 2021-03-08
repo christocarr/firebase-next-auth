e66921f7f10650343af8a22a114e8e237e03b73b
christocarr <christocarr@outlook.com>
2021-03-08 16:26:55 +0000

    fix: add z-index to display dropdown above layout

--- components/Layout/Dropdown.js
+++ components/Layout/Dropdown.js
@@ -1,6 +1,6 @@
 function Dropdown({ children }) {
   return (
-    <div className="flex flex-col absolute top-10 right-6 px-4 py-2 bg-white w-full border border-gray-200 rounded-md shadow-xl">
+    <div className="flex flex-col absolute top-10 right-6 z-50 px-4 py-2 bg-white w-full border border-gray-200 rounded-md shadow-xl">
       {children}
     </div>
   );
