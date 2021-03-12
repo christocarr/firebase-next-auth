65badc5e6a1d5940b360ac2e66f8d61f91ee2957
christocarr <christocarr@outlook.com>
2021-03-12 10:29:40 +0000

    wip: replace button to cancel email and password update with back button to profile page

--- pages/update-profile.js
+++ pages/update-profile.js
@@ -1,10 +1,19 @@
 import Layout from '../components/Layout/Layout';
 import { UpdateEmailPassword } from '../components/Auth';
+import UpdateProfileImage from '../components/UpdateProfileImage';
+import { FormButton } from '../components/Form';
+import { useRouter } from 'next/router';
 
 function UpdateProfile() {
+  const router = useRouter();
+
   return (
     <Layout>
+      <div>
+        <FormButton onClick={() => router.push('/profile')}>Back</FormButton>
+      </div>
       <UpdateEmailPassword />
+      <UpdateProfileImage />
     </Layout>
   );
 }
